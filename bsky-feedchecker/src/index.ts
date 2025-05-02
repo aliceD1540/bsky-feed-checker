765abbdf1d84d0d2e8bdace0abb3ddf4fbbef3f4
grimoire <libra@project-grimoire.dev>
2025-05-03 03:39:28 +0900

    うまく動かないのでawaitにしてみる

--- bsky-feedchecker/src/index.ts
+++ bsky-feedchecker/src/index.ts
@@ -92,18 +92,17 @@ export default {
 		console.log('notExists:', notExists);
 
 		for (const article of notExists) {
-			ctx.waitUntil(
-				bsky_utils
-					.postMessageWithCard('noteで新しい記事を書きました', {
-						title: article.title,
-						link: article.link,
-						thumb_url: article.thumbnail,
-					})
-					.catch((error) => {
-						console.error('Error posting message:', error);
-					})
-			);
+			await bsky_utils
+				.postMessageWithCard('noteで新しい記事を書きました', {
+					title: article.title,
+					link: article.link,
+					thumb_url: article.thumbnail,
+				})
+				.catch((error) => {
+					console.error('Error posting message:', error);
+				});
 		}
+		console.log('Scheduled event processed successfully. notExists.length:', notExists.length);
 	},
 } satisfies ExportedHandler<Env>;
 
